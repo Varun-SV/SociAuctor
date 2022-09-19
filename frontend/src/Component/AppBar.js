102b9bb34e3443ffad0fb7824e4270e1ab6abb16
TechieNK <nk2indian@gmail.com>
2022-09-19 22:25:25 +0530

    Added modal for editing profile

--- frontend/src/Component/AppBar.js
+++ frontend/src/Component/AppBar.js
@@ -16,16 +16,30 @@ import { app } from '../firebase';
 import { useHistory } from 'react-router-dom';
 import {addDoc, collection, getDocs,doc, updateDoc } from "@firebase/firestore";
 import {getFirestore } from "@firebase/firestore";
+import Modal from '@mui/material/Modal';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import { TextField, Autocomplete } from '@mui/material';
+import countryList from 'react-select-country-list';
+import { useMemo } from 'react';
 
 function AppBarWidget() {
   const [fullname, setFullname] = React.useState("");
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [editProfile,setEditProfile] = React.useState(false);
+  const [messageOpen, setmessageOpen] = React.useState(false);   
+  const [message, setMessage] = React.useState(""); 
+  const countryOptions = useMemo(() => countryList().getData(), []);
+  const [userDetails,setUserDetails] = React.useState({email:"",firstName:"",lastName:"",country:""});
+  const [docId,setDocId] = React.useState(null);
   const firebaseAuth = getAuth(app);
   const history = useHistory();
   var ref = null;
   var firestore  = null;
   
+  const auth = getAuth(app);
+
   onAuthStateChanged(firebaseAuth, async(user) => {
     if (user) {
       const uid = user.uid;
@@ -35,13 +49,55 @@ function AppBarWidget() {
       querySnapshot.docs.forEach((doc)=>{
         const dt = doc.data();
         if(dt.userId === uid){
-          setFullname(dt.firstName + ' ' + dt.lastName);
+          setFullname(dt.firstName + ' ' + dt.lastName);          
+          setUserDetails(dt);
+          setDocId(doc.id);
         }
       });
     } else {
       history.push('/login');
     }
   });
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+        return;
+    }
+    setmessageOpen(false);
+  };
+
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+
+  const handleSubmit = async(event)=>{
+    event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const firstName = data.get('firstName');
+        const lastName = data.get('lastName');
+        var country = data.get('country');
+        country = countryList().getValue(country);
+        var ref_ = doc(firestore,"users/"+docId);
+        await updateDoc(ref_,{
+          firstName:firstName,
+          lastName:lastName,
+          email: userDetails.email,
+          userId: userDetails.userId,
+          country: country,
+          wallet_id: userDetails.wallet_id
+        })
+        .then(()=>{
+          setMessage("Profile Updated Successfully");
+          setmessageOpen(true);
+          setEditProfile(false);
+        })
+        .catch((error)=>{
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setMessage(errorMessage);
+          setmessageOpen(true);
+        })
+  }
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -179,7 +235,7 @@ function AppBarWidget() {
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              <MenuItem key='Profile' onClick={handleCloseUserMenu}>
+              <MenuItem key='Profile' onClick={()=>{handleCloseUserMenu();setEditProfile(true)}}>
                   <Typography textAlign="center">Profile</Typography>
               </MenuItem>
               <MenuItem key='Account' onClick={handleCloseUserMenu}>
@@ -194,7 +250,79 @@ function AppBarWidget() {
           </Box>
         </Toolbar>
       </Container>
+      <Modal open={editProfile} onClose={()=>{setEditProfile(false);}} style={{ marginTop: '8%', marginBottom: '20%', height: '65%', width: '100%'}}>
+              <Box sx={style}>
+                    <h1 className="cardTitle">Update your profile</h1>
+                    <br/>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                    <TextField
+                      autoComplete="given-name"
+                      name="firstName"
+                      required
+                      fullWidth
+                      id="firstName"
+                      label="First Name"
+                      defaultValue = {userDetails.firstName}
+                      autoFocus
+                    />
+                    <br />
+                    <br />
+                    <TextField
+                      required
+                      fullWidth
+                      id="lastName"
+                      label="Last Name"
+                      name="lastName"
+                      defaultValue = {userDetails.lastName}
+                      autoComplete="family-name"
+                    />
+                      <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        value = {userDetails.email}
+                        disabled={true}
+                        autoComplete="email"
+                        autoFocus
+                      />
+                      <Autocomplete
+                        disablePortal
+                        id="combo-box-demo"
+                        options={countryOptions}
+                        sx={{ width: '100%', marginTop: '1%' }}
+                        defaultValue={countryList().getLabel(userDetails.country)}
+                        renderInput={(params) => <TextField {...params} label="Select Country" name='country' />}
+                      />
+                      <Button type="submit" style={{width: '100%', background: '#142e36', color: 'white', fontSize: '100%'}} 
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}>Update Profile</Button>
+                      <Snackbar open={messageOpen} autoHideDuration={6000} onClose={handleClose}>
+                          <Alert onClose={handleClose} severity="info">
+                            {message}
+                          </Alert>
+                      </Snackbar>
+                  </Box>
+              </Box>
+      </Modal>
     </AppBar>
   );
 }
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  borderRadius: 2,
+  p: 4,
+  width: '30%',
+  maxHeight: '120%',
+  overflowY: 'scroll'
+};
+
 export default AppBarWidget;
